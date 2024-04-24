750e6de22853a908ff5d3a4e6441d3c8d61d60b1
Alex Zou <lzou4775@uni.sydney.edu.au>
2024-04-24 10:34:25 +1000

    committed auto-generated code

---
+++ pydra/tasks/nireports/reportlets/__init__.py
@@ -0,0 +1,17 @@
+from .modality import plot_heatmap, fMRIPlot
+from .mosaic import (
+    plot_mosaic,
+    plot_segmentation,
+    plot_slice,
+    plot_slice_tern,
+    plot_spikes,
+)
+from .nuisance import (
+    _ward_to_linkage,
+    confoundplot,
+    confounds_correlation_plot,
+    plot_carpet,
+    spikesplot,
+)
+from .utils import _bbox, _get_limits, get_parula
+from .xca import compcor_variance_plot
